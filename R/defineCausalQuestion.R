b78ef9aea9cadc3dc98056bf29ad13e73da30423
Isa Stallworthy <31548151+istallworthy@users.noreply.github.com>
2022-10-10 16:29:59 -0400

    run-through tweaks

--- R/defineCausalQuestion.R
+++ R/defineCausalQuestion.R
@@ -13,7 +13,7 @@ defineCausalQuestion<- function(exposures, exposure_time_pts, outcomes, outcome_
 
   for (i in 1:length(exposures)){
     for (x in 1:length(outcomes)){
-      print(paste0("Goal: examine causal effects of ", exposures[i], " at times ", paste0(exposure_time_pts,sep=",", collapse=""), " on ", outcomes[x], " at time(s) ", outcome_time_pts))
+      print(paste0("Goal: examine causal effects of ", exposures[i], " at times ", paste0(exposure_time_pts,sep=",", collapse=""), " on ", outcomes[x], " at time(s) ", paste(c(outcome_time_pts), sep=",", collapse=",")))
     }}
 
 }
