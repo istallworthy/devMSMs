f46527b7a6c756be9c34a0e0f68b1b1c494ede33
Isa Stallworthy <31548151+istallworthy@users.noreply.github.com>
2023-08-29 16:52:32 -0400

    Update websiteSetup.R

--- R/websiteSetup.R
+++ R/websiteSetup.R
@@ -4,11 +4,13 @@
 #https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#headings
 
 library(pkgdown)
-
 usethis::use_git(message = "Initial commit")
-
 usethis::use_github(private = FALSE)
+usethis::use_github_action("pkgdown")
+usethis::use_pkgdown_github_pages()
 
+# locally building site
+pkgdown::build_site()
 
 usethis::use_pkgdown_github_pages()
 
