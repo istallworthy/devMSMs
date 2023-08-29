0908c33d7420e6e713c992d1ce27d56ae99560ec
Isa Stallworthy <31548151+istallworthy@users.noreply.github.com>
2023-08-29 16:52:12 -0400

    Initial commit

---
+++ R/websiteSetup.R
@@ -0,0 +1,18 @@
+
+#Building devMSMs website
+#https://pkgdown.r-lib.org/articles/pkgdown.html
+#https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#headings
+
+library(pkgdown)
+
+usethis::use_git(message = "Initial commit")
+
+usethis::use_github(private = FALSE)
+
+
+usethis::use_pkgdown_github_pages()
+
+# use_pkgdown_github_pages(config_file = "_pkgdown.yml", destdir = "docs")
+
+pkgdown::build_site()
+devtools::document()
